7ec8c0fc56ff2af71de717c2953edf0235f8c91e
lilykiwi <char73@pm.me>
2025-10-13 20:21:22 +0100

    fix: :bug: remove missing dependency import

--- rollup.config.prod.ts
+++ rollup.config.prod.ts
@@ -1,8 +1,7 @@
-import alias from '@rollup/plugin-alias';
-import nodeResolve from '@rollup/plugin-node-resolve';
-import scss from 'rollup-plugin-scss';
-import typescript from '@rollup/plugin-typescript';
-import del from 'rollup-plugin-delete'
+import alias from '@rollup/plugin-alias'
+import nodeResolve from '@rollup/plugin-node-resolve'
+import scss from 'rollup-plugin-scss'
+import typescript from '@rollup/plugin-typescript'
 
 export default {
   input: 'src/index.tsx',
@@ -11,7 +10,7 @@ export default {
     format: 'es',
     name: 'lilykiwi.xyz',
     sourcemap: false,
-    chunkFileNames: '[name].js'
+    chunkFileNames: '[name].js',
   },
   plugins: [
     del({ targets: 'dist/*.js.map' }),
@@ -20,14 +19,14 @@ export default {
         { find: 'react', replacement: 'preact/compat' },
         { find: 'react-dom/test-utils', replacement: 'preact/test-utils' },
         { find: 'react-dom', replacement: 'preact/compat' },
-        { find: 'react/jsx-runtime', replacement: 'preact/jsx-runtime' }
-      ]
+        { find: 'react/jsx-runtime', replacement: 'preact/jsx-runtime' },
+      ],
     }),
     nodeResolve(),
     typescript(),
     scss({
       fileName: 'bundle.css',
-      outputStyle: 'compressed'
+      outputStyle: 'compressed',
     }),
   ],
-};
+}
